185a90d814f0b188b575ebf87ea757e906bb749d
Ruslan <ruslanmehdiyev922@gmail.com>
2023-02-10 02:13:17 +0400

    format of create date was changed

--- src/pages/Group/Index.jsx
+++ src/pages/Group/Index.jsx
@@ -1,34 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { api } from '../../network/api';
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { api } from "../../network/api";
 
 function Index() {
+  const [groups, setgroups] = useState([]);
 
-    const [groups, setgroups] = useState([]);
+  useEffect(() => {
+    api.getAll("/groups").then((res) => {
+      setgroups(
+        res.map((group) => {
+          return {
+            ...group,
+            createDate: group.createDate.substring(0, 10),
+          };
+        })
+      );
+    });
+  }, []);
 
-    useEffect(() => {
-        api.getAll('/groups')
-            .then(res => {
-                setgroups(res)
-                console.log('Res', res);
-            })
-    }, [])
+  console.log(groups);
+  const columns = [
+    { field: "id", headerName: "Group ID", width: 150 },
+    { field: "name", headerName: "Name", width: 150 },
+    {
+      field: "createDate",
+      headerName: "Create Date",
+      width: 150,
+    },
+  ];
 
-    const columns = [
-        { field: 'id', headerName: 'Group ID', width: 150 },
-        { field: 'name', headerName: 'Name', width: 150 },
-        { field: 'createDate', headerName: 'Create Date', width: 150 },
-      ];
-
-    return (<>
-    <div style={{ height: 300, width: '100%' }}>
-         <DataGrid
-        rows={groups}
-        columns={columns}
-    /> 
-    </div>
-  
-    </>)
+  return (
+    <>
+      <div style={{ height: 300, width: "100%" }}>
+        <DataGrid rows={groups} columns={columns} />
+      </div>
+    </>
+  );
 }
 
-export default Index
+export default Index;
