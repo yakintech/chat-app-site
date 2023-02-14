d56c12d4d2231e5eb6514b429e5008469833fb3c
GulsenZalova <zalova0202@gmail.com>
2023-02-14 18:57:34 +0400

    mail and password validation

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -6,15 +6,19 @@ import ReCAPTCHA from "react-google-recaptcha";
 import "./style.css"
 
 function Login() {
-    const [verified, setVerified] = useState(false)
+    const [verified, setVerified] = useState("")
+    const [email, setEmail] = useState('chagatay.yildiz@code.edu.az')
+    const [password, setPassword] = useState('123')
     function onChange(value) {
         console.log("Captcha value:", value);
+        setVerified(value)
     }
     const navigate = useNavigate();
     const userLogin = () => {
         let user = {
-            email: 'chagatay.yildiz@code.edu.az',
-            password: '123'
+            email,
+            password,
+            response:verified
         }
 
         api.add('/webusers/login', user)
@@ -28,25 +32,36 @@ function Login() {
 
     }
 
+
+    const emailHandler = event =>{
+        setEmail(event.target.value)
+    }
+
+    const passwordHandler = event => {
+        setPassword(event.target.value)
+    }
+
     return (<>
         {/* <button onClick={() => userLogin()}>Login</button> */}
         <div className='container'>
             <div className='formContainer'>
                 <h1>Login Form</h1>
-                <form action="https://validation-microservice.onrender.com/recaptcha/verify" method='POST' className='form'>
+                <form  method='POST' className='form'>
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="text" id='email' />
+                        <input onChange={emailHandler} type="text" id='email' />
                     </div>
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input type="text" id='password' />
+                        <input onChange={passwordHandler} type="text" id='password' />
                     </div>
                     <ReCAPTCHA
                         sitekey="6LcLWlQkAAAAAGGhFyk-RueqPwr4haYD1c8eWpmW"
                         onChange={onChange}
                     />
-                    <button className='submitBtn' onClick={() => userLogin()} type='submit' >Submit</button>
+                    <button className='submitBtn' onClick={(event) =>{
+                        event.preventDefault()
+                        userLogin()}} type='submit' >Submit</button>
                 </form>
             </div>
         </div>
