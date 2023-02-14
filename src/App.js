3a77b4d008656b6289368713340715fe6ff194aa
Ruslan <ruslanmehdiyev922@gmail.com>
2023-02-14 18:25:43 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,59 +1,50 @@
-import React, { useContext } from 'react'
-import ChatPage from './pages/ChatPage'
-import Create from './pages/group/Create'
-import { Routes, Route, Navigate, useNavigate, Link } from 'react-router-dom'
-import Index from './pages/group/Index'
-import Login from './pages/public/Login'
-import { authContext } from './store/AuthContext'
-import ConfirmCode from './pages/public/ConfirmCode'
-import Home from './pages/dashboard/Home'
-import PublicLayout from './pages/layout/PublicLayout'
-import ProtecrtedLayout from './pages/layout/ProtectedLayout'
-import ProtectedLayout from './pages/layout/ProtectedLayout'
+import React, { useContext } from "react";
+import ChatPage from "./pages/ChatPage";
+import Create from "./pages/Group/Create";
+import { Routes, Route, Navigate, useNavigate, Link } from "react-router-dom";
+import Index from "./pages/Group/Index";
+import Login from "./pages/public/Login";
+import { authContext } from "./store/AuthContext";
+import ConfirmCode from "./pages/public/ConfirmCode";
+import Home from "./pages/dashboard/Home";
+import PublicLayout from "./pages/layout/PublicLayout";
+import ProtecrtedLayout from "./pages/layout/ProtectedLayout";
+import ProtectedLayout from "./pages/layout/ProtectedLayout";
 
 function App() {
-
-  return (<>
-
-
-    {/*  PublicLayout -> public pages   */}
-    {/* protected layout -> private pages */}
+  return (
+    <>
+      {/*  PublicLayout -> public pages   */}
+      {/* protected layout -> private pages */}
 
       <Routes>
-
         <Route element={<PublicLayout />}>
-          <Route path='/' element={<Login />} />
-          <Route path='confirmCode' element={<ConfirmCode />} />
+          <Route path="/" element={<Login />} />
+          <Route path="confirmCode" element={<ConfirmCode />} />
         </Route>
 
-        <Route path='/admin' element={<ProtectedLayout />}>
-          <Route path='home' element={<Home />}></Route>
-          <Route path='group/create' element={<Create />} />
-          <Route path='group' element={<Index />} />
+        <Route path="/admin" element={<ProtectedLayout />}>
+          <Route path="home" element={<Home />}></Route>
+          <Route path="group/create" element={<Create />} />
+          <Route path="group" element={<Index />} />
         </Route>
-
       </Routes>
-
-
-
-
-  </>
-  )
+    </>
+  );
 }
 
 function Auth({ children }) {
-
   const { loginStatus } = useContext(authContext);
 
   if (loginStatus) {
-    return children
-  }
-  else {
-    return <Routes>
-      <Route path='/' element={<Login />} />
-    </Routes>
+    return children;
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    );
   }
 }
 
-
-export default App
+export default App;
--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -1,32 +1,33 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { api } from '../../network/api';
-import { authContext } from '../../store/AuthContext';
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../network/api";
+import { authContext } from "../../store/AuthContext";
 
 function Login() {
+  const navigate = useNavigate();
+  const userLogin = () => {
+    let user = {
+      email: "chagatay.yildiz@code.edu.az",
+      password: "123",
+    };
 
-    const navigate = useNavigate();
-    const userLogin = () => {
+    api
+      .add("/webusers/login", user)
+      .then((res) => {
+        console.log(res);
+        navigate("confirmCode", { state: { webUserId: res._id } });
+      })
+      .catch((err) => {
+        console.log("Err", err);
+        alert("Email or password invalid!");
+      });
+  };
 
-        let user = {
-            email: 'chagatay.yildiz@code.edu.az',
-            password: '123'
-        }
-
-        api.add('/webusers/login', user)
-            .then(res => {
-                navigate('confirmCode', { state: { webUserId: res._id } })
-            })
-            .catch(err => {
-                console.log('Err', err);
-                alert('Email or password invalid!')
-            })
-
-    }
-
-    return (<>
-        <button onClick={() => userLogin()}>Login</button>
-    </>)
+  return (
+    <>
+      <button onClick={() => userLogin()}>Login</button>
+    </>
+  );
 }
 
-export default Login
+export default Login;
