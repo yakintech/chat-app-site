e3a6ab4e9435bb2462b9916db87b9d6c5b30adcc
Gul Akberova <kbrovagul0@gmail.com>
2023-02-15 10:14:35 +0400

    sidebar css

--- src/pages/layout/ProtectedLayout.jsx
+++ src/pages/layout/ProtectedLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../../components/Sidebar';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import styles from './index.module.css';
 
 function ProtectedLayout() {
