b501d9089840c315f39bc6e63c3e48bb017d017e
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-02-16 13:43:30 +0300

    group create bug fix

--- src/App.js
+++ src/App.js
@@ -1,58 +1,8 @@
-<<<<<<< HEAD
-import React, { useContext } from 'react';
-import ChatPage from './pages/ChatPage';
-import Create from './pages/Group/Create';
-import { Routes, Route, Navigate, useNavigate, Link } from 'react-router-dom';
-import Index from './pages/Group/Index';
-import Login from './pages/public/Login';
-import { authContext } from './store/AuthContext';
-import ConfirmCode from './pages/public/ConfirmCode';
-import Home from './pages/dashboard/Home';
-import PublicLayout from './pages/layout/PublicLayout';
-import ProtecrtedLayout from './pages/layout/ProtectedLayout';
-import ProtectedLayout from './pages/layout/ProtectedLayout';
-import Dashboard from './pages/dashboard/Dashboard';
-
-function App() {
-	return (
-		<>
-			{/*  PublicLayout -> public pages   */}
-			{/* protected layout -> private pages */}
-
-			<Routes>
-				<Route element={<PublicLayout />}>
-					<Route path="/" element={<Login />} />
-					<Route path="confirmCode" element={<ConfirmCode />} />
-				</Route>
-
-				<Route path="/admin" element={<ProtectedLayout />}>
-					<Route path="dashboard" element={<Dashboard />} />
-					<Route path="group/create" element={<Create />} />
-					<Route path="group" element={<Index />} />
-				</Route>
-			</Routes>
-		</>
-	);
-}
-
-function Auth({ children }) {
-	const { loginStatus } = useContext(authContext);
-
-	if (loginStatus) {
-		return children;
-	} else {
-		return (
-			<Routes>
-				<Route path="/" element={<Login />} />
-			</Routes>
-		);
-	}
-=======
 import React, { useContext } from "react";
 import ChatPage from "./pages/ChatPage";
-import Create from "./pages/Group/Create";
+import Create from "./pages/group/Create";
 import { Routes, Route, Navigate, useNavigate, Link } from "react-router-dom";
-import Index from "./pages/Group/Index";
+import Index from "./pages/group/Index";
 import Login from "./pages/public/Login";
 import { authContext } from "./store/AuthContext";
 import ConfirmCode from "./pages/public/ConfirmCode";
@@ -95,7 +45,6 @@ function Auth({ children }) {
       </Routes>
     );
   }
->>>>>>> main
 }
 
 export default App;
