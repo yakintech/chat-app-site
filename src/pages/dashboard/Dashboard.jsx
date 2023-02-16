f916e1ffc3af8f29b694c18e30460e0379094648
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-02-17 00:31:21 +0400

    add groups and users length,get users names

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './index.module.css';
 import group from '../../images/icons/networking.png';
 import user from '../../images/icons/user.png';
@@ -7,15 +7,32 @@ import offline from '../../images/icons/offline.png';
 import profile from '../../images/icons/profile.png';
 import groupuser from '../../images/icons/groupuser.png';
 import ApexChatr from '../../components/react-apexCharts/ApexChatr';
+import { api } from '../../network/api';
 
 function Dashboard() {
+	
+	const [groups, setGroups]=useState([]);
+	const [users,setUsers]=useState([]);
+
+	useEffect(() => {
+		api.getAll('/groups').then((res) => {
+			setGroups(res);
+		});
+	}, []);
+
+	useEffect(() => {
+		api.getAll('/webUsers').then((res) => {
+			setUsers(res);
+		});
+	}, []);
+	console.log(users);
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.info_boxs}>
 				<div className={styles.info_box}>
 					<div className={styles.left}>
 						<h2 className={styles.title}>number of groups</h2>
-						<p className={styles.count}>7</p>
+						<p className={styles.count}>{groups.length}</p>
 					</div>
 					<div className={styles.right}>
 						<img src={group} alt="#" />
@@ -24,7 +41,7 @@ function Dashboard() {
 				<div className={styles.info_box}>
 					<div className={styles.left}>
 						<h2 className={styles.title}>users</h2>
-						<p className={styles.count}>29</p>
+						<p className={styles.count}>{users.length}</p>
 					</div>
 					<div className={styles.right}>
 						<img src={user} alt="#" />
@@ -53,7 +70,7 @@ function Dashboard() {
 			<div className={styles.chat}>
 				<div className={styles.new_user}>
 					<div className={styles.user_title_wrapper}>
-						<h2 className={styles.user_title}>New Users</h2>
+						<h2 className={styles.user_title}>All Users</h2>
 						<img className={styles.user_img} src={groupuser} alt="#" />
 					</div>
 
@@ -62,26 +79,13 @@ function Dashboard() {
 						<h3 className={styles.user_subtitle}>Name</h3>
 						<h3 className={styles.user_subtitle}>Date</h3>
 					</div>
-					<div className={styles.user_info}>
+                     {users.map((user)=>(
+						<div className={styles.user_info} key={user._id}>
 						<img src={profile} alt="#" />
-						<p className={styles.user_name}>Eminem</p>
+						<p className={styles.user_name}>{user.email}</p>
 						<p className={styles.user_date}>01.02.2023</p>
 					</div>
-					<div className={styles.user_info}>
-						<img src={profile} alt="#" />
-						<p className={styles.user_name}>Eminem</p>
-						<p className={styles.user_date}>01.02.2023</p>
-					</div>
-					<div className={styles.user_info}>
-						<img src={profile} alt="#" />
-						<p className={styles.user_name}>Eminem</p>
-						<p className={styles.user_date}>01.02.2023</p>
-					</div>
-					<div className={styles.user_info}>
-						<img src={profile} alt="#" />
-						<p className={styles.user_name}>Eminem</p>
-						<p className={styles.user_date}>01.02.2023</p>
-					</div>
+					 ))}
 				</div>
 
 				<ApexChatr />
